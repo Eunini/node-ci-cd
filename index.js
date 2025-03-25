bdc482d1a952a3e2e51074a1bac184feb5755194
eunice <atandainioluwa371@gmail.com>
2025-03-25 07:20:57 -0700

    Added another CI/CD workflow

--- index.js
+++ index.js
@@ -7,3 +7,4 @@ app.get("/", (req, res) => {
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//  ssh -i "C:\Users\USER\.ssh\LandingPageVM_key.pem" azureuser@128.85.44.34
